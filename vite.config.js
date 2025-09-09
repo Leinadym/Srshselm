c8d8cf7a9d9df995f830dcf7f9f506ce3b05cdcd
Daniel Zabat Moreno <164299290+Leinadym@users.noreply.github.com>
2025-09-10 01:43:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    base: '/', // ✅ para tama ang asset path sa production
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-});
+})
